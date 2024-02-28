2bcef893e0fa323c3bab740eca9685d9969cd777
Carlos <102172522+carlosmp15@users.noreply.github.com>
2024-02-28 19:09:18 +0100

    Add files via upload

--- JavaComponent/src/main/java/es/localhost/javacomponent/HelloController.java
+++ JavaComponent/src/main/java/es/localhost/javacomponent/HelloController.java
@@ -1,44 +1,74 @@
 package es.localhost.javacomponent;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
-    @FXML TextArea txtParametros;
-    @FXML TextArea txtRespuesta;
-    @FXML Button btBEnviarConsulta;
-    @FXML ComboBox listaOpciones;
-    @FXML TextField Ruta;
-    private Operador op = new Operador();
+    private final String[] operacion = {"Insertar una casa", "Insertar una persona",
+            "Modificar una casa", "Modificar una persona", "Eliminar una casa",
+            "Eliminar una persona"};
+    @FXML
+    private ComboBox<String> cbOperacion;
+    @FXML
+    private Label lTitulo;
+
+    @FXML
+    private void onComboClick(){
+        if(cbOperacion.getValue().equals(operacion[0])){
+            loadNewScene("insert-house.fxml", lTitulo, "Insert House" );
+        } else if(cbOperacion.getValue().equals(operacion[1])){
+            loadNewScene("insert-person.fxml", lTitulo, "Insert Person" );
+        } else if(cbOperacion.getValue().equals(operacion[2])){
+            loadNewScene("update-house.fxml", lTitulo, "Update House" );
+        } else if(cbOperacion.getValue().equals(operacion[3])){
+            loadNewScene("update-person.fxml", lTitulo, "Update Person" );
+        } else if(cbOperacion.getValue().equals(operacion[4])){
+            loadNewScene("remov-house.fxml", lTitulo, "Remove Person" );
+        } else if(cbOperacion.getValue().equals(operacion[5])){
+            loadNewScene("remov-house.fxml", lTitulo, "Remove House" );
+        }
+    }
+
+    public static void loadNewScene(String nScene, Label label, String title) {
+        try {
+            FXMLLoader loader = new FXMLLoader(HelloController.class.getResource(nScene));
+            Parent root = loader.load();
+
+            // Get the current Stage
+            Stage stage = (Stage) label.getScene().getWindow();
+            stage.setTitle(title);
+
+            // Create a new scene
+            Scene scene = new Scene(root);
+
+            // Set the new scene to the Stage
+            stage.setScene(scene);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        ObservableList<String> items = FXCollections.observableArrayList();
-        items.addAll("POST", "PUT", "GET", "DELETE");listaOpciones.setItems(items);
-        listaOpciones.setValue("POST");
+        cbOperacion.getItems().addAll(operacion);
+        //cbOperacion.setValue(operacion[0]);
     }
-    @FXML
-    public void cxxt(){
-        if(listaOpciones.getValue().toString().compareTo("POST") == 0 || listaOpciones.getValue().toString().compareTo("PUT") == 0)txtParametros.setDisable(false);
-        else txtParametros.setDisable(true);
-    }
-    @FXML
-    public void EjecutarConsulta(){
-        switch (listaOpciones.getValue().toString()){
-            case "POST":txtRespuesta.setText(op.post(Ruta.getText(),txtParametros.getText()));break;
-            case "PUT":txtRespuesta.setText(op.put(Ruta.getText(),txtParametros.getText()));break;
-            case "GET":txtRespuesta.setText(op.get(Ruta.getText()));break;
-            case "DELETE":txtRespuesta.setText(op.delete(Ruta.getText()));break;
-        }
+
+    private void showAlert(String title, String textHeader, String textContent, Alert.AlertType alertType) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(textHeader);
+        alert.setContentText(textContent);
+        alert.showAndWait();
     }
 }
--- JavaComponent/src/main/java/module-info.java
+++ JavaComponent/src/main/java/module-info.java
@@ -4,8 +4,6 @@ module es.localhost.javacomponent {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
-    requires org.apache.httpcomponents.httpcore;
-    requires org.apache.httpcomponents.httpclient;
 
     opens es.localhost.javacomponent to javafx.fxml;
     exports es.localhost.javacomponent;
