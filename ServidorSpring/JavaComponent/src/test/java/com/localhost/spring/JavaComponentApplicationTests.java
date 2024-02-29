b28456986029e5feb34b5687818b369f87978ecb
Carlos <102172522+carlosmp15@users.noreply.github.com>
2024-02-29 18:26:00 +0100

    servidor-funcionando

---
+++ ServidorSpring/JavaComponent/src/test/java/com/localhost/spring/JavaComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localhost.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaComponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
