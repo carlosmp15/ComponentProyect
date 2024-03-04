2be05e78dcb5159f8f98c9efab6df05da3db5371
Carlos <102172522+carlosmp15@users.noreply.github.com>
2024-03-04 17:27:32 +0100

    Delete ServidorSpring directory

--- ServidorSpring/src/main/java/com/localhost/spring/repositories/IPersonReporitory.java
+++
@@ -1,8 +0,0 @@
-package com.localhost.spring.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.localhost.spring.models.PersonModel;
-
-public interface IPersonReporitory extends JpaRepository<PersonModel, Long> {
-
-}
